c211dae3ce8863fb0c074f7bfe98494a250d5e45
tiefseetauchner <lena@tauchner.me>
2024-08-30 21:12:18 +0200

    - update testconsole

--- Pdfer.TestConsole/Program.cs
+++ Pdfer.TestConsole/Program.cs
@@ -17,7 +17,9 @@ class Program
     pdfDocument.Trailer.TrailerDictionary.ToList().ForEach(entry => Console.WriteLine($"trailer entry: {entry.Key} {entry.Value}"));
     Console.WriteLine($"startxref offset: {pdfDocument.Trailer.XRefByteOffset}");
     pdfDocument.XRefTable.ToList().ForEach(entry => Console.WriteLine($"xref entry: {entry.Key} {entry.Value}"));
-    
+
+    pdfDocument.Body.Objects.ToList().ForEach(obj => Console.WriteLine($"body object: {obj}"));
+
     Console.Write("Press any key to exit...");
     Console.ReadKey();
     Console.WriteLine();
