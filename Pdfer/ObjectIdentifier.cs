8e2599570828f31fcd32e7cbe50dba5614ee635b
Lena Tauchner <lena.tauchner@rubicon.eu>
2024-12-03 09:11:23 +0100

    - add tryparse to ObjectIdentifier

--- Pdfer/ObjectIdentifier.cs
+++ Pdfer/ObjectIdentifier.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 
 namespace Pdfer;
@@ -6,10 +7,52 @@ public record ObjectIdentifier(
   int ObjectNumber,
   int Generation)
 {
-  public static ObjectIdentifier ParseReference(string objectIdentifier)
+  public static ObjectIdentifier ParseReference(string objectReference)
   {
-    var objectIdentifierParts = objectIdentifier.Split(' ');
-    return new ObjectIdentifier(int.Parse(objectIdentifierParts[0]), int.Parse(objectIdentifierParts[1]));
+    if (TryParseReference(objectReference, out var parsedReference))
+      return parsedReference;
+
+    throw new ArgumentException($"'{objectReference}' is not a valid object reference.", nameof(objectReference));
+  }
+
+  public static bool TryParseReference(string objectReference, out ObjectIdentifier parsedReference)
+  {
+    parsedReference = null!;
+
+    var objectReferenceParts = objectReference.Trim().Split(' ');
+
+    if (objectReferenceParts.Length != 3)
+      return false;
+
+    if (!int.TryParse(objectReferenceParts[0], out var number) || !int.TryParse(objectReferenceParts[1], out var generation) || objectReferenceParts[2] != "R")
+      return false;
+
+    parsedReference = new ObjectIdentifier(number, generation);
+    return true;
+  }
+
+  public static ObjectIdentifier ParseIdentifier(string objectIdentifier)
+  {
+    if (TryParseIdentifier(objectIdentifier, out var parsedIdentifier))
+      return parsedIdentifier;
+
+    throw new ArgumentException($"'{objectIdentifier}' is not a valid object identifier.", nameof(objectIdentifier));
+  }
+
+  public static bool TryParseIdentifier(string objectReference, out ObjectIdentifier parsedReference)
+  {
+    parsedReference = null!;
+
+    var objectReferenceParts = objectReference.Trim().Split(' ');
+
+    if (objectReferenceParts.Length != 3)
+      return false;
+
+    if (!int.TryParse(objectReferenceParts[0], out var number) || !int.TryParse(objectReferenceParts[1], out var generation) || objectReferenceParts[2] != "obj")
+      return false;
+
+    parsedReference = new ObjectIdentifier(number, generation);
+    return true;
   }
 
   public byte[] GetHeaderBytes() =>
