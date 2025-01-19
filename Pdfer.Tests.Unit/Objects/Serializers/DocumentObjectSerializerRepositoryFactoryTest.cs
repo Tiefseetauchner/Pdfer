6578054271304c77dcd91b7b95060259dd1bbd73
tiefseetauchner <lena@tauchner.me>
2025-01-19 06:35:37 +0100

    - improve DocumentObjectSerializerRepositoryFactoryTest

--- Pdfer.Tests.Unit/Objects/Serializers/DocumentObjectSerializerRepositoryFactoryTest.cs
+++ Pdfer.Tests.Unit/Objects/Serializers/DocumentObjectSerializerRepositoryFactoryTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Pdfer.Common.Tests;
 using Pdfer.Objects.Serializers;
 
 namespace Pdfer.Tests.Unit.Objects.Serializers;
@@ -7,5 +8,17 @@ public class DocumentObjectSerializerRepositoryFactoryTest
 {
   [Test]
   public void CreateForAllSerializers() =>
-    Assert.That(DocumentObjectSerializerRepositoryFactory.CreateForAllSerializers(), Is.TypeOf<DocumentObjectSerializerRepository>());
+    TypeAssert.VerifyInstanceOf<DocumentObjectSerializerRepository>(DocumentObjectSerializerRepositoryFactory.CreateForAllSerializers(), repository =>
+    {
+      TypeAssert.VerifyInstanceOf<ArrayObjectSerializer>(repository.GetSerializer(ObjectBuilder.ArrayObject()));
+      TypeAssert.VerifyInstanceOf<BooleanObjectSerializer>(repository.GetSerializer(ObjectBuilder.BooleanObject(true)));
+      TypeAssert.VerifyInstanceOf<DictionaryObjectSerializer>(repository.GetSerializer(ObjectBuilder.DictionaryObject()));
+      TypeAssert.VerifyInstanceOf<ReferenceObjectSerializer>(repository.GetSerializer(ObjectBuilder.ReferenceObject()));
+      TypeAssert.VerifyInstanceOf<NameObjectSerializer>(repository.GetSerializer(ObjectBuilder.NameObject("Test")));
+      TypeAssert.VerifyInstanceOf<NullObjectSerializer>(repository.GetSerializer(ObjectBuilder.NullObject()));
+      TypeAssert.VerifyInstanceOf<NumericObjectSerializer>(repository.GetSerializer(ObjectBuilder.NumericObject(1)));
+      TypeAssert.VerifyInstanceOf<NumericObjectSerializer>(repository.GetSerializer(ObjectBuilder.NumericObject(1.25)));
+      TypeAssert.VerifyInstanceOf<StreamObjectSerializer>(repository.GetSerializer(ObjectBuilder.StreamObject()));
+      TypeAssert.VerifyInstanceOf<StringObjectSerializer>(repository.GetSerializer(ObjectBuilder.StringObject("Test")));
+    });
 }
